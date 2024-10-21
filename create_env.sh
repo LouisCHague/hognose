eea5337a3cc63553096b893181d0df177eb054c0
LouisCHague <louishague18@gmail.com>
2024-10-21 14:19:36 +0100

    fixed file pathing

--- create_env.sh
+++ create_env.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+# Location of the Hognose DIR
+HOG_DIR=$(dirname "$0")
+
 # Check if the directory exists, if not, create it
 if [ ! -d "$1" ]; then
-  mkdir -p "$1/bin" "$1/lib" "$1/include"
+  mkdir -p "$HOG_DIR/$1/bin" "$HOG_DIR/$1/lib" "$HOG_DIR/$1/include"
 else
   echo "$1 already exists."
   exit 1
 fi
 
 # Create the activate.sh script
-cat <<EOL > "$1/activate.sh"
+cat <<EOL > "$HOG_DIR/$1/activate.sh"
 #!/bin/bash
-export VIRTUAL_ENV="/mnt/c/Users/louis/hognose/$1"
+export VIRTUAL_ENV="$HOG_DIR/$1"
 export PATH="\$VIRTUAL_ENV/bin:\$PATH"
 export PS1="($1) \$PS1"
 EOL
 
 # Create the deactivate.sh script 
 # Unsets the virtual env, removes it from $PATH and removes (env)
-# NEED TO MAKE IT SO /mnt/c/Users/louis/hognose... is relative to the USER!!!!!
-cat <<EOL > "$1/deactivate.sh"
+cat <<EOL > "$HOG_DIR/$1/deactivate.sh"
 #!/bin/bash
 unset VIRTUAL_ENV
-export PATH=\$(echo "\$PATH" | sed -e 's|:/mnt/c/Users/louis/hognose/$1/bin||; s|^/mnt/c/Users/louis/hognose/$1/bin:||; s|^/mnt/c/Users/louis/hognose/$1/bin$||')
+export PATH=\$(echo "\$PATH" | sed -e 's|:$HOG_DIR/$1/bin||; s|^$HOG_DIR/$1/bin:||; s|^$HOG_DIR/$1/bin$||')
 export PS1=\$(echo "\$PS1" | sed "s|($1) ||")
 EOL
